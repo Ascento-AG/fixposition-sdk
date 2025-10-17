89aa5062b079755c3f61b9f5121eb3b47bb8dcc2
Giuseppe Rizzi <giuseppe.rizzi@ascento.ch>
2025-10-17 14:00:38 +0200

    fix clang compilation errors

--- fpsdk_common/include/fpsdk_common/ext/eigen_core.hpp
+++ fpsdk_common/include/fpsdk_common/ext/eigen_core.hpp
@@ -1,23 +1,80 @@
 // Wrapper to suppress warning from Eigen headers
-#ifndef __FPSDK_COMMON_EXT_EIGEN_CORE_HPP__
-#define __FPSDK_COMMON_EXT_EIGEN_CORE_HPP__
+#ifndef __FPSDK_COMMON_EXT_EIGEN_GEOMETRY_HPP__
+#define __FPSDK_COMMON_EXT_EIGEN_GEOMETRY_HPP__
+
+// Make __has_warning available which clang provides; define a safe fallback for other compilers
+#ifndef __has_warning
+#  define __has_warning(x) 0
+#endif
+
+// -------------------------
+// Enter "quiet zone" for Eigen
+// -------------------------
+#if defined(__clang__)
+
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wall"
+#pragma clang diagnostic ignored "-Wextra"
+#pragma clang diagnostic ignored "-Wpedantic"
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wshadow"
+
+#if __has_warning("-Wclass-memaccess")
+#pragma clang diagnostic ignored "-Wclass-memaccess"
+#endif
+#if __has_warning("-Wmaybe-uninitialized")
+#pragma clang diagnostic ignored "-Wmaybe-uninitialized"
+#endif
+#if __has_warning("-Wdeprecated-copy")
+#pragma clang diagnostic ignored "-Wdeprecated-copy"
+#endif
+
+#include <Eigen/Geometry>
+#pragma clang diagnostic pop
+
+#elif defined(__GNUC__) || defined(__GNUG__)
+
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wall"
 #pragma GCC diagnostic ignored "-Wextra"
 #pragma GCC diagnostic ignored "-Wpedantic"
 #pragma GCC diagnostic ignored "-Wunused-parameter"
 #pragma GCC diagnostic ignored "-Wshadow"
-#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"  // NOLINT
-#if defined(__GNUC__) && (__GNUC__ >= 9)
-#  pragma GCC diagnostic ignored "-Wdeprecated-copy"
+
+#pragma GCC diagnostic ignored "-Wclass-memaccess"
+
+#if (__GNUC__ >= 9)
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+#pragma GCC diagnostic ignored "-Wdeprecated-copy"
 #endif
-#pragma GCC diagnostic ignored "-Wclass-memaccess"  // NOLINT
-#include <Eigen/Core>
+
+#include <Eigen/Geometry>
 #pragma GCC diagnostic pop
-// For older Eigen (e.g. 3.3.7, which we have in fusion-dev-env and on the sensor), we unfortunately have to disable
-// this warning globally (templates instantiation can be anywhere...). :-/ GCC 9 (in Yocto) doesn't like this.
-// See https://gitlab.com/libeigen/eigen/-/issues/1788, https://gitlab.com/libeigen/eigen/-/merge_requests/29
-#if !EIGEN_VERSION_AT_LEAST(3, 4, 0) && defined(__GNUC__) && (__GNUC__ >= 9)
-#  pragma GCC diagnostic ignored "-Wdeprecated-copy"
+#else
+
+// Fallback
+#include <Eigen/Geometry>
+
 #endif
-#endif  // __FPSDK_COMMON_EXT_EIGEN_CORE_HPP__
+
+// -------------------------
+// Post-include special case
+// -------------------------
+// See explanation in eigen_core.hpp
+// For Eigen < 3.4, the library can still trigger -Wdeprecated-copy in user code.
+// Keep the original behavior: suppress it outside the push/pop when necessary.
+#if !EIGEN_VERSION_AT_LEAST(3, 4, 0)
+
+#if defined(__clang__)
+#  if __has_warning("-Wdeprecated-copy")
+#    pragma clang diagnostic ignored "-Wdeprecated-copy"
+#  endif
+#elif defined(__GNUC__) || defined(__GNUG__)
+#  if (__GNUC__ >= 9)
+#    pragma GCC diagnostic ignored "-Wdeprecated-copy"
+#  endif
+#endif
+
+#endif  // !EIGEN_VERSION_AT_LEAST(3, 4, 0)
+
+#endif  // __FPSDK_COMMON_EXT_EIGEN_GEOMETRY_HPP__
--- fpsdk_common/src/parser/types.cpp
+++ fpsdk_common/src/parser/types.cpp
@@ -108,7 +108,7 @@ void ParserMsg::MakeInfo() const
             constexpr int num = 16;
             sinfo[0] = '\0';
             for (int ix = 0; (ix < num) && (ix < msize); ix++) {
-                std::snprintf(&sinfo[ix * 3], sizeof(sinfo) - (ix * 3), "%02x ", mdata[ix]);
+                std::snprintf(&sinfo[ix * 3], MAX_INFO_SIZE - (ix * 3), "%02x ", mdata[ix]);
             }
             if (msize > 16) {
                 sinfo[(num * 3) + 0] = '.';
